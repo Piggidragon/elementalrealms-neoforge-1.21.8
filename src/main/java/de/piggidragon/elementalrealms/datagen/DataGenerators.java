e3706131e731edf0a6be8b09c5e1ba1c260e569f
Piggidragon <inf24119@lehre.dhbw-stuttgart.de>
2025-10-01 00:08:31 +0200

    feature: added DataGenerators

---
+++ src/main/java/de/piggidragon/elementalrealms/datagen/DataGenerators.java
@@ -0,0 +1,17 @@
+package de.piggidragon.elementalrealms.datagen;
+
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.data.event.GatherDataEvent;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.PackOutput;
+
+@EventBusSubscriber(modid = "deinmodid")
+public class DataGenerators {
+    @SubscribeEvent
+    public static void gatherClientData(GatherDataEvent.Client event) {
+        DataGenerator generator = event.getGenerator();
+        PackOutput packOutput = generator.getPackOutput();
+        generator.addProvider(true, new ModModelProvider(packOutput));
+    }
+}
