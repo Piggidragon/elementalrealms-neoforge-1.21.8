163a898b9e55ddd3a65bb84d6319dfd3ab829f7f
Simeon <inf24119@lehre.dhbw-stuttgart.de>
2025-09-30 18:20:14 +0200

    feat: - added random affinity assignment on world creation - added set, get, has affinity methods

--- src/main/java/de/piggidragon/elementalrealms/magic/affinities/Affinity.java
+++ src/main/java/de/piggidragon/elementalrealms/magic/affinities/Affinity.java
@@ -3,6 +3,9 @@ package de.piggidragon.elementalrealms.magic.affinities;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.MapCodec;
 
+import java.util.Arrays;
+import java.util.List;
+
 public enum Affinity {
     NONE(AffinityType.NONE),
     FIRE(AffinityType.ELEMENTAL),
@@ -21,13 +24,34 @@ public enum Affinity {
             Codec.STRING.xmap(Affinity::valueOf, Affinity::name);
     public static final MapCodec<Affinity> MAP_CODEC =
             CODEC.fieldOf("affinity");
+
     private final AffinityType type;
 
     Affinity(AffinityType affinityType) {
         this.type = affinityType;
     }
 
+    public static List<Affinity> getAllElemental() {
+        return Arrays.stream(Affinity.values())
+                .filter(a -> a.getType() == AffinityType.ELEMENTAL)
+                .toList();
+    }
+
     public AffinityType getType() {
         return type;
+    }
+
+    public Affinity getDeviant(Affinity affinity) {
+        if (getType() == AffinityType.ELEMENTAL) {
+            return switch (affinity) {
+                case FIRE -> LIGHTNING;
+                case WATER -> ICE;
+                case WIND -> SOUND;
+                case EARTH -> GRAVITY;
+                default -> NONE;
+            };
+        } else {
+            return NONE;
+        }
     }
 }
--- src/main/java/de/piggidragon/elementalrealms/magic/affinities/ModAffinities.java
+++ src/main/java/de/piggidragon/elementalrealms/magic/affinities/ModAffinities.java
@@ -3,9 +3,12 @@ package de.piggidragon.elementalrealms.magic.affinities;
 import de.piggidragon.elementalrealms.ElementalRealms;
 import de.piggidragon.elementalrealms.attachments.ModAttachments;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.util.RandomSource;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.entity.player.PlayerEvent;
+
+import java.util.List;
 
 @EventBusSubscriber(modid = ElementalRealms.MODID)
 public class ModAffinities {
@@ -13,21 +16,47 @@ public class ModAffinities {
         ElementalRealms.LOGGER.info("Registering Mod Affinities for " + ElementalRealms.MODID);
     }
 
-    public static void setAffinity(ServerPlayer player, Affinity affinity) {
-        player.setData(ModAttachments.AFFINITY, affinity);
+    public static void addAffinity(ServerPlayer player, Affinity affinity) {
+        List<Affinity> affinities = getAffinities(player);
+        if (!affinities.contains(affinity)) {
+            affinities.add(affinity);
+        }
     }
 
-    public static Affinity getAffinity(ServerPlayer player) {
-        return player.getData(ModAttachments.AFFINITY);
+    public static List<Affinity> getAffinities(ServerPlayer player) {
+        return player.getData(ModAttachments.AFFINITIES.get());
     }
+
+    public static boolean hasAffinity(ServerPlayer player, Affinity affinity) {
+        return getAffinities(player).contains(affinity);
+    }
+
+    private static Affinity randomElementalAffinity(ServerPlayer player, int probabilityPercent) {
+        RandomSource random = player.getRandom();
+
+        List<Affinity> current = getAffinities(player);
+        List<Affinity> available = Affinity.getAllElemental().stream()
+                .filter(a -> !hasAffinity(player, a))
+                .toList();
+
+        if (available.isEmpty()) return Affinity.NONE;
+
+        if (random.nextInt(100) >= probabilityPercent) return Affinity.NONE;
+
+        return available.get(random.nextInt(available.size()));
+    }
+
 
     @SubscribeEvent
     public static void onPlayerLogin(PlayerEvent.PlayerLoggedInEvent event) {
-        ElementalRealms.LOGGER.info("Logged in: " + event.getEntity().getName());
-    }
-
-    @SubscribeEvent
-    public static void onPlayerClone(PlayerEvent.Clone event) {
-        ElementalRealms.LOGGER.info("Died: " + event.getEntity().getName());
+        if (event.getEntity() instanceof ServerPlayer player) {
+            addAffinity(player, randomElementalAffinity(player, 100));
+            for (int x : new int[]{25, 10, 20}) {
+                Affinity newAffinity = randomElementalAffinity(player, x);
+                if (newAffinity != Affinity.NONE) {
+                    addAffinity(player, newAffinity);
+                }
+            }
+        }
     }
 }
