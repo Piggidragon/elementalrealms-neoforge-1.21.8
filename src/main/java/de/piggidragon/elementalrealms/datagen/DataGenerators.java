c7493d6e3b6cd2d9bf069d8e905bc5907daee0b1
Piggidragon <inf24119@lehre.dhbw-stuttgart.de>
2025-10-06 10:36:38 +0200

    feat: added shards and essences to model provider fix: -fixed typo in en_us.json -fixed wrong definition in AffinityRecipeProvider

--- src/main/java/de/piggidragon/elementalrealms/datagen/DataGenerators.java
+++ src/main/java/de/piggidragon/elementalrealms/datagen/DataGenerators.java
@@ -1,10 +1,14 @@
 package de.piggidragon.elementalrealms.datagen;
 
+import de.piggidragon.elementalrealms.datagen.magic.affinities.AffinityRecipeProvider;
+import net.minecraft.core.HolderLookup;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
+
+import java.util.concurrent.CompletableFuture;
 
 @EventBusSubscriber(modid = "elementalrealms")
 public class DataGenerators {
@@ -12,6 +16,9 @@ public class DataGenerators {
     public static void gatherClientData(GatherDataEvent.Client event) {
         DataGenerator generator = event.getGenerator();
         PackOutput packOutput = generator.getPackOutput();
+        CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
+
         generator.addProvider(true, new ModModelProvider(packOutput));
+        generator.addProvider(true, new AffinityRecipeProvider.Runner(packOutput, lookupProvider));
     }
 }
--- src/main/java/de/piggidragon/elementalrealms/datagen/ModModelProvider.java
+++ src/main/java/de/piggidragon/elementalrealms/datagen/ModModelProvider.java
@@ -16,16 +16,49 @@ public class ModModelProvider extends ModelProvider {
     protected void registerModels(BlockModelGenerators blockModels, ItemModelGenerators itemModels) {
 
         itemModels.generateFlatItem(AffinityItems.AFFINITY_STONE_FIRE.get(), ModelTemplates.FLAT_ITEM);
+        itemModels.generateFlatItem(AffinityItems.AFFINITY_SHARD_FIRE.get(), ModelTemplates.FLAT_ITEM);
+        itemModels.generateFlatItem(AffinityItems.ESSENCE_FIRE.get(), ModelTemplates.FLAT_ITEM);
+
         itemModels.generateFlatItem(AffinityItems.AFFINITY_STONE_WATER.get(), ModelTemplates.FLAT_ITEM);
+        itemModels.generateFlatItem(AffinityItems.AFFINITY_SHARD_WATER.get(), ModelTemplates.FLAT_ITEM);
+        itemModels.generateFlatItem(AffinityItems.ESSENCE_WATER.get(), ModelTemplates.FLAT_ITEM);
+
         itemModels.generateFlatItem(AffinityItems.AFFINITY_STONE_WIND.get(), ModelTemplates.FLAT_ITEM);
+        itemModels.generateFlatItem(AffinityItems.AFFINITY_SHARD_WIND.get(), ModelTemplates.FLAT_ITEM);
+        itemModels.generateFlatItem(AffinityItems.ESSENCE_WIND.get(), ModelTemplates.FLAT_ITEM);
+
         itemModels.generateFlatItem(AffinityItems.AFFINITY_STONE_EARTH.get(), ModelTemplates.FLAT_ITEM);
+        itemModels.generateFlatItem(AffinityItems.AFFINITY_SHARD_EARTH.get(), ModelTemplates.FLAT_ITEM);
+        itemModels.generateFlatItem(AffinityItems.ESSENCE_EARTH.get(), ModelTemplates.FLAT_ITEM);
+
         itemModels.generateFlatItem(AffinityItems.AFFINITY_STONE_LIGHTNING.get(), ModelTemplates.FLAT_ITEM);
+        itemModels.generateFlatItem(AffinityItems.AFFINITY_SHARD_LIGHTNING.get(), ModelTemplates.FLAT_ITEM);
+        itemModels.generateFlatItem(AffinityItems.ESSENCE_LIGHTNING.get(), ModelTemplates.FLAT_ITEM);
+
         itemModels.generateFlatItem(AffinityItems.AFFINITY_STONE_ICE.get(), ModelTemplates.FLAT_ITEM);
+        itemModels.generateFlatItem(AffinityItems.AFFINITY_SHARDE_ICE.get(), ModelTemplates.FLAT_ITEM);
+        itemModels.generateFlatItem(AffinityItems.ESSENCE_ICE.get(), ModelTemplates.FLAT_ITEM);
+
         itemModels.generateFlatItem(AffinityItems.AFFINITY_STONE_SOUND.get(), ModelTemplates.FLAT_ITEM);
+        itemModels.generateFlatItem(AffinityItems.AFFINITY_SHARD_SOUND.get(), ModelTemplates.FLAT_ITEM);
+        itemModels.generateFlatItem(AffinityItems.ESSENCE_SOUND.get(), ModelTemplates.FLAT_ITEM);
+
         itemModels.generateFlatItem(AffinityItems.AFFINITY_STONE_GRAVITY.get(), ModelTemplates.FLAT_ITEM);
+        itemModels.generateFlatItem(AffinityItems.AFFINITY_SHARD_GRAVITY.get(), ModelTemplates.FLAT_ITEM);
+        itemModels.generateFlatItem(AffinityItems.ESSENCE_GRAVITY.get(), ModelTemplates.FLAT_ITEM);
+
         itemModels.generateFlatItem(AffinityItems.AFFINITY_STONE_TIME.get(), ModelTemplates.FLAT_ITEM);
+        itemModels.generateFlatItem(AffinityItems.AFFINITY_SHARD_TIME.get(), ModelTemplates.FLAT_ITEM);
+        itemModels.generateFlatItem(AffinityItems.ESSENCE_TIME.get(), ModelTemplates.FLAT_ITEM);
+
         itemModels.generateFlatItem(AffinityItems.AFFINITY_STONE_SPACE.get(), ModelTemplates.FLAT_ITEM);
+        itemModels.generateFlatItem(AffinityItems.AFFINITY_SHARD_SPACE.get(), ModelTemplates.FLAT_ITEM);
+        itemModels.generateFlatItem(AffinityItems.ESSENCE_SPACE.get(), ModelTemplates.FLAT_ITEM);
+
         itemModels.generateFlatItem(AffinityItems.AFFINITY_STONE_LIFE.get(), ModelTemplates.FLAT_ITEM);
+        itemModels.generateFlatItem(AffinityItems.AFFINITY_SHARD_LIFE.get(), ModelTemplates.FLAT_ITEM);
+        itemModels.generateFlatItem(AffinityItems.ESSENCE_LIFE.get(), ModelTemplates.FLAT_ITEM);
+
         itemModels.generateFlatItem(AffinityItems.AFFINITY_STONE_VOID.get(), ModelTemplates.FLAT_ITEM);
 
         /*
