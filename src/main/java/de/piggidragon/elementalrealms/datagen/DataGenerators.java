819e3278bd3877a0ac179a34a6780565c647d9ae
Piggidragon <inf24119@lehre.dhbw-stuttgart.de>
2025-10-07 14:48:23 +0200

    feat: added PortalBlockModelProvider

--- src/main/java/de/piggidragon/elementalrealms/datagen/DataGenerators.java
+++ src/main/java/de/piggidragon/elementalrealms/datagen/DataGenerators.java
@@ -1,7 +1,9 @@
 package de.piggidragon.elementalrealms.datagen;
 
+import de.piggidragon.elementalrealms.datagen.dimensions.PortalBlockModelProvider;
 import de.piggidragon.elementalrealms.datagen.dimensions.SchoolDimensionProvider;
 import de.piggidragon.elementalrealms.datagen.magic.affinities.AffinityRecipeProvider;
+import de.piggidragon.elementalrealms.datagen.magic.affinities.AffinityItemModelProvider;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
@@ -19,8 +21,9 @@ public class DataGenerators {
         PackOutput packOutput = generator.getPackOutput();
         CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
 
-        generator.addProvider(true, new ModModelProvider(packOutput));
+        generator.addProvider(true, new AffinityItemModelProvider(packOutput));
+        generator.addProvider(true, new PortalBlockModelProvider(packOutput));
         generator.addProvider(true, new AffinityRecipeProvider.Runner(packOutput, lookupProvider));
-        generator.addProvider(false, new SchoolDimensionProvider());
+        generator.addProvider(true, new SchoolDimensionProvider());
     }
 }
