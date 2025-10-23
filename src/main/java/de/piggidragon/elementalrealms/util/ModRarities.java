30df89f5074ac96d2484fae238b1f3e2f7dd4920
Piggidragon <inf24119@lehre.dhbw-stuttgart.de>
2025-10-23 11:49:55 +0200

    fix: fixed custom rarities not working

--- src/main/java/de/piggidragon/elementalrealms/util/ModRarities.java
+++ src/main/java/de/piggidragon/elementalrealms/util/ModRarities.java
@@ -1,5 +1,6 @@
-package de.piggidragon.elementalrealms.rarities;
+package de.piggidragon.elementalrealms.util;
 
+import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Style;
 import net.minecraft.world.item.Rarity;
 import net.neoforged.fml.common.asm.enumextension.EnumProxy;
@@ -10,15 +11,15 @@ public class ModRarities {
     public static final EnumProxy<Rarity> LEGENDARY = new EnumProxy<>(
             Rarity.class,
             -1,
-            "elemtentalrealms:legendary",
-            (UnaryOperator<Style>) style -> style.withColor(0xFFD700).withBold(true)
+            "elementalrealms:legendary",
+            (UnaryOperator<Style>) style -> style.withColor(ChatFormatting.GOLD)
     );
 
     public static final EnumProxy<Rarity> MYTHIC = new EnumProxy<>(
             Rarity.class,
             -1,
-            "elemtentalrealms:mythic",
-            (UnaryOperator<Style>) style -> style.withColor(0xFF00FF).withBold(true).withItalic(true)
+            "elementalrealms:mythic",
+            (UnaryOperator<Style>) style -> style.withColor(ChatFormatting.DARK_PURPLE)
     );
 
 }
