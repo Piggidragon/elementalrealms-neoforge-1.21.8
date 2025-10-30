a2d5c62bb06638ed82145bf1c2fff62f2c706a94
Piggidragon <inf24119@lehre.dhbw-stuttgart.de>
2025-10-30 20:04:52 +0100

    doc: cleared DataGenerators docs

--- src/main/java/de/piggidragon/elementalrealms/datagen/DataGenerators.java
+++ src/main/java/de/piggidragon/elementalrealms/datagen/DataGenerators.java
@@ -22,8 +22,6 @@ public class DataGenerators {
     /**
      * Event handler that registers all data generators (both client and server).
      * Called automatically during the data generation phase of the build process.
-     *
-     * @param event The client data gathering event containing generator and lookup providers
      */
     @SubscribeEvent
     public static void onGatherData(GatherDataEvent.Client event) {
@@ -31,10 +29,8 @@ public class DataGenerators {
         PackOutput packOutput = generator.getPackOutput();
         CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
 
-        // Client-side providers (models, textures)
         generator.addProvider(true, new ModModelProvider(packOutput));
 
-        // Server-side providers (recipes, advancements)
         generator.addProvider(true, new AffinityRecipeProvider.Runner(packOutput, lookupProvider));
 
         generator.addProvider(true, new ModAdvancementProvider(
@@ -42,9 +38,6 @@ public class DataGenerators {
                 lookupProvider,
                 List.of(new AdvancementGenerator())
         ));
-
-        //generator.addProvider(true, new ModWorldGenProvider(packOutput, lookupProvider));
-
     }
 
 }
