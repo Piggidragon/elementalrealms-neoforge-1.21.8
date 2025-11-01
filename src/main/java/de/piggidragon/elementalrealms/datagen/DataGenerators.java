6c12f4647400e87111b537806bf519042f6c2a41
Piggidragon <inf24119@lehre.dhbw-stuttgart.de>
2025-11-01 22:47:03 +0100

    feat: added custom configured feature to DataGenerators

--- src/main/java/de/piggidragon/elementalrealms/datagen/DataGenerators.java
+++ src/main/java/de/piggidragon/elementalrealms/datagen/DataGenerators.java
@@ -38,6 +38,8 @@ public class DataGenerators {
                 lookupProvider,
                 List.of(new AdvancementGenerator())
         ));
+
+        event.createDatapackRegistryObjects(ModFeaturesProvider.createBuilder());
     }
 
 }
