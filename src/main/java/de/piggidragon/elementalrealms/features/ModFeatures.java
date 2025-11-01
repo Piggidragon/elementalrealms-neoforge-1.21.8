2bfc9480314cb282cf10b3b3aa3f7ad8e8646697
Piggidragon <inf24119@lehre.dhbw-stuttgart.de>
2025-11-01 22:45:31 +0100

    fix: fixed custom feature registration

--- src/main/java/de/piggidragon/elementalrealms/features/ModFeatures.java
+++ src/main/java/de/piggidragon/elementalrealms/features/ModFeatures.java
@@ -1,22 +1,23 @@
 package de.piggidragon.elementalrealms.features;
 
 import de.piggidragon.elementalrealms.ElementalRealms;
+import de.piggidragon.elementalrealms.features.config.PortalConfiguration;
+import de.piggidragon.elementalrealms.features.custom.PortalSpawnFeature;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.level.levelgen.feature.Feature;
-import net.minecraft.world.level.levelgen.feature.configurations.NoneFeatureConfiguration;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 import java.util.function.Supplier;
 
 public class ModFeatures {
-    // In ModFeatures
     public static final DeferredRegister<Feature<?>> FEATURES =
             DeferredRegister.create(Registries.FEATURE, ElementalRealms.MODID);
 
-    public static final Supplier<Feature<NoneFeatureConfiguration>> PORTAL_FEATURE =
+    public static final Supplier<Feature<PortalConfiguration>> PORTAL_FEATURE =
             FEATURES.register("portal_feature", () ->
-                    new PortalSpawnFeature(NoneFeatureConfiguration.CODEC));
+                    new PortalSpawnFeature(PortalConfiguration.CODEC));
+
 
     /**
      * Registers all mod features with the mod event bus.
